3c7391c2286fc36e87344d4d51396d0b113604a2
spectrumtic <info@spectrumtic.com>
2020-11-13 00:54:48 +0100

    Agregar archivos de proyecto.

---
+++ TricomBowling.CoreVS2017/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TricomBowling.CoreVS2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
