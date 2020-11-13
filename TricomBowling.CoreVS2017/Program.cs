01a953ea2c3c1f69d0b23c51875ef0b8be77e139
spectrumtic <info@spectrumtic.com>
2020-11-13 02:14:15 +0100

    Add test with Spare/Score total

--- TricomBowling.CoreVS2017/Program.cs
+++ TricomBowling.CoreVS2017/Program.cs
@@ -4,10 +4,42 @@ namespace TricomBowling.CoreVS2017
 {
     public class Game
     {
+        int _score = 0;
+        int[] pinFalls = new int[21];
+        int rollsCounter;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+        }
+
+        public void Roll(int pins)
+        {
+            //_score += pins;
+            pinFalls[rollsCounter] = pins;
+            rollsCounter++;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int i = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (pinFalls[i] + pinFalls[i + 1] == 10)
+                {
+                    score += 10 + pinFalls[i + 2];
+                    i++;
+                }
+                else
+                {
+                    score += pinFalls[i] + pinFalls[i+1];
+                    i++;
+                }
+            }
+            return score;
         }
     }
 }
--- TricomBowling.CoreVS2017_Test/UnitTest1.cs
+++ TricomBowling.CoreVS2017_Test/UnitTest1.cs
@@ -5,15 +5,53 @@ namespace TricomBowling.CoreVS2017_Test
 {
     public class Tests
     {
+        Game game;
+        
         [SetUp]
         public void Setup()
         {
+            game = new Game();
+        }
+
+        void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pins);
+            }
+
         }
 
         [Test]
-        public void CreateGame()
+        public void RollGame()
         {
-            var game = new Game();
+            RollMany(20, 0);
+            Assert.That(game.Score(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void RollOnes()
+        {
+            RollMany(20, 1);
+            Assert.That(game.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void RollSpareFirstFrame()
+        {
+            game.Roll(9);
+            game.Roll(1);
+            RollMany(18, 1);
+
+            Assert.That(game.Score(), Is.EqualTo(29));
+        }
+
+        [Test]
+        public void RollSparesAllFrame()
+        {
+            RollMany(21, 5);
+
+            Assert.That(game.Score(), Is.EqualTo(150));
         }
     }
 }
